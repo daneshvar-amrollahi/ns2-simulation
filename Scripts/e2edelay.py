b94c18c92563d09122e36e99e659a94c18adb295
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2022-04-29 12:36:06 +0430

    Modified scripts

--- Scripts/e2edelay.py
+++ Scripts/e2edelay.py
@@ -14,7 +14,6 @@ for line in lines[10:]:
     words = line.split()    
 
     trace_level = words[3]
-    packet_size = int(words[7])
     payload_type = words[6]
     current_node = words[2]
     event_tpye = words[0]
@@ -25,19 +24,19 @@ for line in lines[10:]:
         continue
 
     if words[0] == "s" and words[2] == sender_node:
-        sent[(sender_node, packet_id, payload_type, trace_level, packet_size)] = time
+        sent[(sender_node, packet_id, payload_type, trace_level)] = time
 
-    if words[0] == "r" and (sender_node, packet_id, payload_type, trace_level, packet_size) in sent:
-        delay = time - sent[(sender_node, packet_id, payload_type, trace_level, packet_size)]
+    if words[0] == "r" and (sender_node, packet_id, payload_type, trace_level) in sent:
+        delay = time - sent[(sender_node, packet_id, payload_type, trace_level)]
 
-        print(sent[(sender_node, packet_id, payload_type, trace_level, packet_size)], time)
+        # print(sent[(sender_node, packet_id, payload_type, trace_level, packet_size)], time)
 
         total_delay += delay
         cnt += 1
 
 
-print(f"Total Delay from {receiver_node} to {sender_node} = {total_delay}")
+print(f"Total Delay from {sender_node} to {receiver_node} = {total_delay}s")
 print(f"Number of packets transferred from {sender_node} and {receiver_node} = {cnt}")
-print(f"Average End-to-End Delay = {total_delay/cnt}")
+print(f"Average End-to-End Delay = {total_delay/cnt}s")
 
 # How 2 run: python e2edelay.py 1p5mbps/wireless.tr 0 7
